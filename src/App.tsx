3b39d13e20c08893d6386d3853f653f6abc22057
David Reed <davidreed@PaczkiNet.local>
2020-11-29 22:33:25 -0500

    pulled command bar out to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Canvas } from "./Canvas";
-import { Tooltip } from "antd";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
-import {
-  faPlay,
-  faPause,
-  faStepBackward,
-  faStepForward,
-  faFastBackward,
-  faFastForward,
-  faFileVideo,
-} from "@fortawesome/free-solid-svg-icons";
 import {
   createBlobFromDataURL,
   endStream,
@@ -23,6 +12,7 @@ import {
 } from "./utils";
 import { ISettings } from "./main";
 import mime from "mime-types";
+import { CommandBar } from "./components/CommandBar";
 
 function useRefState(
   initialState: any
@@ -193,74 +183,14 @@ export function App({
   return (
     <div>
       <Canvas width={width} height={height} setCanvasProps={setCanvasProps} />
-      {settings.animation && (
-        <div className="mx-auto text-center mt-5">
-          <span className="border rounded px-2 py-2 select-none">
-            <span className="font-mono">
-              Frame
-              <span
-                style={{ minWidth: "5rem" }}
-                className="inline-block text-right"
-              >
-                {frame.current}/{settings.totalFrames}
-              </span>
-            </span>
-
-            <FontAwesomeIcon
-              className="fill-current text-gray-500 hover:text-gray-600 cursor-pointer mx-1"
-              onClick={() => setFrame(0)}
-              icon={faFastBackward}
-            />
-            <FontAwesomeIcon
-              className="fill-current text-gray-500 hover:text-gray-600 cursor-pointer mx-1"
-              onClick={() =>
-                setFrame(frame.current > 0 ? frame.current - 1 : frame.current)
-              }
-              icon={faStepBackward}
-            />
-            {isPlaying ? (
-              <FontAwesomeIcon
-                className="fill-current text-gray-500 hover:text-gray-600 cursor-pointer mx-1"
-                onClick={() => setIsPlaying(false)}
-                icon={faPause}
-              />
-            ) : (
-              <FontAwesomeIcon
-                className="fill-current text-gray-500 hover:text-gray-600 cursor-pointer mx-1"
-                onClick={() =>
-                  setIsPlaying(frame.current < settings.totalFrames)
-                }
-                icon={faPlay}
-              />
-            )}
-            <FontAwesomeIcon
-              className="fill-current text-gray-500 hover:text-gray-600 cursor-pointer mx-1"
-              onClick={() =>
-                setFrame(
-                  frame.current < settings.totalFrames
-                    ? frame.current + 1
-                    : frame.current
-                )
-              }
-              icon={faStepForward}
-            />
-
-            <FontAwesomeIcon
-              className="fill-current text-gray-500 hover:text-gray-600 cursor-pointer mx-1"
-              onClick={() => setFrame(settings.totalFrames)}
-              icon={faFastForward}
-            />
-
-            <Tooltip title="Record Animation">
-              <FontAwesomeIcon
-                className="fill-current text-gray-500 hover:text-gray-600 cursor-pointer mx-1"
-                onClick={() => record()}
-                icon={faFileVideo}
-              />
-            </Tooltip>
-          </span>
-        </div>
-      )}
+      <CommandBar
+        settings={settings}
+        frame={frame}
+        setFrame={setFrame}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        record={record}
+      />
     </div>
   );
 }
